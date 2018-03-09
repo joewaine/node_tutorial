16436ca380d8babc54e88b4eed2e347ec9370041
Joe Waine <joseph@comradeagency.com>
2018-03-09 11:17:29 -0800

    node_tutorialgit add .

--- router.js
+++ router.js
@@ -1,15 +1,13 @@
-function route(handle, pathname, response, postData){
-  console.log('about to route a request for' + pathname);
-if(typeof handle[pathname] === 'function'){
-  return handle[pathname](response, postData);
-}else{
-  console.log('no request handler found for' +  pathname);
-  response.writeHead(404, {"content-type": "text/plain"});
-  response.write("404 not found");
-  response.end();
+function route(handle, pathname, response, request) {
+console.log("About to route a request for " + pathname);
+if (typeof handle[pathname] === 'function') {
+handle[pathname](response, request);
+} else {
+console.log("No request handler found for " + pathname);
+response.writeHead(404, {"Content-Type": "text/html"});
+response.write("404 Not found");
+response.end();
 }
-
-
 }
 
 exports.route = route;
